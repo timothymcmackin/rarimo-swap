d6c4d399777a2da8e0228f0023b651d5f9dfaf47
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-06 10:38:29 -0400

    Take some things from the SDK source to calculate the output amount correctly

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { ChainNames, BridgeChain, Amount } from '@rarimo/shared'
+import { ChainNames, BridgeChain, Amount, RARIMO_BRIDGE_FEE, NATIVE_TOKEN_WRAP_SLIPPAGE_MULTIPLIER } from '@rarimo/shared'
 import {createProvider} from '@rarimo/provider'
 import {MetamaskProvider} from '@rarimo/providers-evm'
 import {
@@ -43,7 +43,7 @@ const test = async () => {
 
     const selectedChain = chains.find((i: BridgeChain) => i.name === ChainNames.Goerli)!
 
-    // Initialize the swap operation
+    // Initialize the swap operation and set the amount of the source token to swap
     const opInitParams:CheckoutOperationParams = {
       chainIdFrom: selectedChain.id,
       chainIdTo: selectedChain.id,
@@ -71,19 +71,25 @@ const test = async () => {
     }
     const destinationToken:Token = newToken(destinationTokenOpts)
 
+    const chainTo = swapper.chains.find(
+      i => Number(i.id) === Number(opInitParams.chainIdTo),
+    )
+
     const paymentTokens = await op.loadPaymentTokens(selectedChain)
     const selectedToken = paymentTokens?.find(i => i.symbol === sourceToken.symbol)!
     const estimatedPrice:EstimatedPrice = await op.estimatePrice(selectedToken)
 
-    // The amount of the source token to swap
-    const sourceAmount:Amount = Amount.fromRaw("0.01", 18)
+    // Get the amount you'll get out of the swap
+    const { amountIn, amountOut } = getAmounts(opInitParams, estimatedPrice)
 
     const swapParams:ExecuteArgs = {
-      amountIn: sourceAmount,
+      amountIn,
       from: sourceToken,
       to: destinationToken,
-      amountOut: estimatedPrice, // Do I have to calculate this myself?
-      path: estimatedPrice.path, // Don't know what this path is
+      amountOut,
+      chainTo,
+      isWrapped: true,
+      path: estimatedPrice.path, // What is this path?
     }
 
     // Run the swap
@@ -93,4 +99,54 @@ const test = async () => {
     console.error(e)
   }
 }
+
+// Copied these functions from packages/nft-checkout/src/operations/evm/evm-operation.ts and helpers.js
+const getNativeAmountIn = (
+  params: CheckoutOperationParams,
+  rawPrice: Price | Amount,
+): Amount => {
+  if (isSameChainOperation(params)) return rawPrice
+
+  const amountWithSlippage = BN.fromBigInt(
+    rawPrice.value,
+    rawPrice.decimals,
+  ).mul(BN.fromRaw(NATIVE_TOKEN_WRAP_SLIPPAGE_MULTIPLIER, rawPrice.decimals))
+
+  return Amount.fromBigInt(amountWithSlippage.value, rawPrice.decimals)
+}
+
+const getAmounts = (
+  params: CheckoutOperationParams,
+  e: EstimatedPrice,
+): { amountIn: Amount; amountOut: Amount } => {
+  const amountIn = e.from.isNative
+    ? getNativeAmountIn(params, e.price)
+    : e.price
+
+  const amountOut = getSwapAmount(params)
+
+  return { amountIn, amountOut }
+}
+
+import { BN } from '@distributedlab/tools'
+
+const getSwapAmount = (params: CheckoutOperationParams): Amount => {
+  const rawPrice = params.price
+  if (isSameChainOperation(params)) return rawPrice
+
+  const decimals = rawPrice.decimals
+  const numerator = BN.fromBigInt(rawPrice.value, decimals)
+
+  const percentBN = BN.fromRaw(RARIMO_BRIDGE_FEE, decimals).div(
+    BN.fromRaw(100, decimals),
+  )
+
+  const denominator = BN.fromRaw(1, decimals).sub(percentBN)
+
+  return Amount.fromBigInt(numerator.div(denominator).value, rawPrice.decimals)
+}
+const isSameChainOperation = (params: CheckoutOperationParams) => {
+  return Number(params.chainIdFrom) === Number(params.chainIdTo)
+}
+
 </script>
