928a5c8f9b45d0ef360ef081c4e162988a86ef43
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-08 14:36:11 -0400

    Don't use the swap package directly

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div>
-      Source network: {{ selectedChainName }}
+      Source network: {{ sourceChainName }}
     </div>
     <div style="margin-top:20px">
       Destination network: {{ destinationChainName }}
     </div>
 
-    <button style="margin:46px 0" @click="test">Swap</button>
+    <button style="margin:46px 0" @click="sendSwapTransaction">Swap</button>
 
     <code>
     </code>
@@ -15,141 +15,55 @@
 </template>
 
 <script setup lang="ts">
-import { ChainNames, BridgeChain, Amount, RARIMO_BRIDGE_FEE, NATIVE_TOKEN_WRAP_SLIPPAGE_MULTIPLIER } from '@rarimo/shared'
-import {createProvider} from '@rarimo/provider'
-import {MetamaskProvider} from '@rarimo/providers-evm'
-import {
-  Price,
-  createCheckoutOperation,
-  EVMOperation,
-  CheckoutOperationParams,
-  EstimatedPrice,
-} from '@rarimo/nft-checkout'
-import { createEVMSwapper, createSwapper, ExecuteArgs } from '@rarimo/swap'
-import { Token, newToken, NewTokenOpts } from '@rarimo/bridge'
+import { ChainNames } from '@rarimo/shared'
+import { CheckoutOperationParams, createCheckoutOperation, EVMOperation, Price } from '@rarimo/nft-checkout'
+import { createProvider } from '@rarimo/provider'
+import { MetamaskProvider } from '@rarimo/providers-evm'
 
-const selectedChainName = ChainNames.Goerli
+const sourceChainName = ChainNames.Goerli
 const destinationChainName = ChainNames.Goerli
 
-const test = async () => {
+const sendSwapTransaction = async () => {
+  // Connect to the Metamask wallet in the browser, using the MetamaskProvider interface to limit bundle size.
+  const provider = await createProvider(MetamaskProvider)
 
-  try {
+  // Initialize the object that represents the transaction operation, in this case on EVM.
+  const op = createCheckoutOperation(EVMOperation, provider)
 
-    // Initialize the wallet provider, swapper, and operation
-    const provider = await createProvider(MetamaskProvider)
-    const swapper = createSwapper(createEVMSwapper, provider)
-    await swapper.init()
-    const op = createCheckoutOperation(EVMOperation, provider)
-    const chains = await op.supportedChains()
+  // Get the chains that are supported from that chain type.
+  const chains = await op.supportedChains()
 
-    const selectedChain = chains.find((i: BridgeChain) => i.name === ChainNames.Goerli)!
-    const destinationChain = chains.find((i: BridgeChain) => i.name === ChainNames.Goerli)!
+  // Select the source and destination chains.
+  // This example uses the Goerli chain, but your application can ask the user which chain to use.
+  const sourceChain = chains.find(i => i.name === sourceChainName)
+  const destinationChain = chains.find(i => i.name === destinationChainName)
 
-    // Initialize the swap operation and set the amount of the source token to swap
-    const opInitParams:CheckoutOperationParams = {
-      chainIdFrom: selectedChain.id,
-      chainIdTo: destinationChain.id,
-      price: Price.fromRaw("0.01", 18, "ETH"),
-    }
-    await op.init(opInitParams)
+  console.log(sourceChain)
+  console.log(destinationChain)
 
-    // Source token type, in this case UNI
-    const sourceTokenOpts: NewTokenOpts = {
-      chain: selectedChain,
-      name: "Uniswap",
-      symbol: "UNI",
-      address: provider.address!,
-      decimals: 18,
-    }
-    const sourceToken = newToken(sourceTokenOpts)
-
-    // Destination token type
-    const destinationTokenOpts: NewTokenOpts = {
-      chain: destinationChain,
-      name: "Wrapped Ether",
-      symbol: "WETH",
-      address: provider.address!,
-      decimals: 18,
-    }
-    const destinationToken:Token = newToken(destinationTokenOpts)
-
-    // Not sure why I need this separate object for the target chain; why can't I use the existing object for the destination chain?
-    const chainTo = swapper.chains.find(
-      i => Number(i.id) === Number(opInitParams.chainIdTo),
-    )
-
-    const paymentTokens = await op.loadPaymentTokens(selectedChain)
-    const selectedToken = paymentTokens?.find(i => i.symbol === sourceToken.symbol)!
-    const estimatedPrice:EstimatedPrice = await op.estimatePrice(selectedToken)
-
-    // Get the amount you'll get out of the swap
-    const { amountIn, amountOut } = getAmounts(opInitParams, estimatedPrice)
-
-    const swapParams:ExecuteArgs = {
-      amountIn,
-      from: sourceToken,
-      to: destinationToken,
-      amountOut,
-      chainTo,
-      isWrapped: true,
-      path: estimatedPrice.path, // What is this path?
-    }
-
-    // Run the swap
-    await swapper.execute(swapParams)
-
-  } catch (e) {
-    console.error(e)
+  // Configure the swap transaction
+  const swapParams: CheckoutOperationParams = {
+    // Source and destination chains
+    chainIdFrom: sourceChain,
+    chainIdTo: destinationChain,
+    // Address to send the swapped tokens to
+    recipient: provider.address!.toString(),
+    // Amount of tokens to receive
+    price: Price.fromRaw("0.1", 18, "WETH"),
+    // The token to swap the source token to, in this case WETH
+    swapTargetTokenSymbol: "WETH",
   }
+
+  // Initialize the transaction
+  await op.init(swapParams)
+
+  // Identify the tokens to exchange, in this case UNI
+  const tokens = await op.loadPaymentTokens(sourceChain!)
+  const paymentToken = tokens.find(({ symbol }) => symbol === "UNI")
+  const estimatedPrice = await op.estimatePrice(paymentToken!)
+
+  // Run the transaction
+  // The `checkout()` method takes the parameters from the operation instance, gets approval from the user's wallet, and calls the Rarimo contract to handle the transaction.
+  const txHash = await op.checkout(estimatedPrice)
 }
-
-// Copied these functions from packages/nft-checkout/src/operations/evm/evm-operation.ts and helpers.js
-const getNativeAmountIn = (
-  params: CheckoutOperationParams,
-  rawPrice: Price | Amount,
-): Amount => {
-  if (isSameChainOperation(params)) return rawPrice
-
-  const amountWithSlippage = BN.fromBigInt(
-    rawPrice.value,
-    rawPrice.decimals,
-  ).mul(BN.fromRaw(NATIVE_TOKEN_WRAP_SLIPPAGE_MULTIPLIER, rawPrice.decimals))
-
-  return Amount.fromBigInt(amountWithSlippage.value, rawPrice.decimals)
-}
-
-const getAmounts = (
-  params: CheckoutOperationParams,
-  e: EstimatedPrice,
-): { amountIn: Amount; amountOut: Amount } => {
-  const amountIn = e.from.isNative
-    ? getNativeAmountIn(params, e.price)
-    : e.price
-
-  const amountOut = getSwapAmount(params)
-
-  return { amountIn, amountOut }
-}
-
-import { BN } from '@distributedlab/tools'
-
-const getSwapAmount = (params: CheckoutOperationParams): Amount => {
-  const rawPrice = params.price
-  if (isSameChainOperation(params)) return rawPrice
-
-  const decimals = rawPrice.decimals
-  const numerator = BN.fromBigInt(rawPrice.value, decimals)
-
-  const percentBN = BN.fromRaw(RARIMO_BRIDGE_FEE, decimals).div(
-    BN.fromRaw(100, decimals),
-  )
-
-  const denominator = BN.fromRaw(1, decimals).sub(percentBN)
-
-  return Amount.fromBigInt(numerator.div(denominator).value, rawPrice.decimals)
-}
-const isSameChainOperation = (params: CheckoutOperationParams) => {
-  return Number(params.chainIdFrom) === Number(params.chainIdTo)
-}
-
 </script>
