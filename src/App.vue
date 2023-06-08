cb0e8ffbc1dde2ae5d49922dcdd940f1f2f30c75
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-08 14:50:10 -0400

    Trying to work out what tokens to use

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <div>
-      Source network: {{ sourceChainName }}
+      Source network: {{ sourceChainName }}<br/>
+      Source token: {{ sourceTokenSymbol }}
     </div>
     <div style="margin-top:20px">
-      Destination network: {{ destinationChainName }}
+      Destination network: {{ destinationChainName }}<br/>
+      Destination token: {{ destinationTokenSymbol }}
     </div>
 
     <button style="margin:46px 0" @click="sendSwapTransaction">Swap</button>
@@ -16,12 +18,15 @@
 
 <script setup lang="ts">
 import { ChainNames } from '@rarimo/shared'
-import { CheckoutOperationParams, createCheckoutOperation, EVMOperation, Price } from '@rarimo/nft-checkout'
+import { CheckoutOperationParams, createCheckoutOperation, EVMOperation, Price, BridgeChain } from '@rarimo/nft-checkout'
 import { createProvider } from '@rarimo/provider'
 import { MetamaskProvider } from '@rarimo/providers-evm'
 
 const sourceChainName = ChainNames.Goerli
 const destinationChainName = ChainNames.Goerli
+
+const sourceTokenSymbol = "UNI"
+const destinationTokenSymbol = "WETH";
 
 const sendSwapTransaction = async () => {
   // Connect to the Metamask wallet in the browser, using the MetamaskProvider interface to limit bundle size.
@@ -35,23 +40,20 @@ const sendSwapTransaction = async () => {
 
   // Select the source and destination chains.
   // This example uses the Goerli chain, but your application can ask the user which chain to use.
-  const sourceChain = chains.find(i => i.name === sourceChainName)
-  const destinationChain = chains.find(i => i.name === destinationChainName)
+  const sourceChain: BridgeChain = chains.find(i => i.name === sourceChainName)!
+  const destinationChain: BridgeChain = chains.find(i => i.name === destinationChainName)!
 
-  console.log(sourceChain)
-  console.log(destinationChain)
+  console.log('Swapping', sourceTokenSymbol, 'for', destinationTokenSymbol)
 
   // Configure the swap transaction
   const swapParams: CheckoutOperationParams = {
     // Source and destination chains
-    chainIdFrom: sourceChain,
-    chainIdTo: destinationChain,
+    chainIdFrom: sourceChain.id,
+    chainIdTo: destinationChain.id,
     // Address to send the swapped tokens to
     recipient: provider.address!.toString(),
     // Amount of tokens to receive
-    price: Price.fromRaw("0.1", 18, "WETH"),
-    // The token to swap the source token to, in this case WETH
-    swapTargetTokenSymbol: "WETH",
+    price: Price.fromRaw("0.1", 18, destinationTokenSymbol),
   }
 
   // Initialize the transaction
@@ -59,7 +61,7 @@ const sendSwapTransaction = async () => {
 
   // Identify the tokens to exchange, in this case UNI
   const tokens = await op.loadPaymentTokens(sourceChain!)
-  const paymentToken = tokens.find(({ symbol }) => symbol === "UNI")
+  const paymentToken = tokens.find(({ symbol }) => symbol === sourceTokenSymbol)
   const estimatedPrice = await op.estimatePrice(paymentToken!)
 
   // Run the transaction
